35c1d6978174da949165d0883a3c90aedb36a887
unknown <marcosgonzalezpenin@gmail.com>
2022-06-20 09:35:08 +0200

    Incorporación flatmap

--- src/main/java/ej59_flatMap/Main59.java
+++ src/main/java/ej59_flatMap/Main59.java
@@ -1,16 +1,23 @@
 package ej59_flatMap;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Main59 {
 	public static void main(String[] args) {
 
-		Stream<String> nombres = Stream.of("PEDRO", "ANA", "MARIA", "JUAN");
-	
-	
-	
-		
-		
-		
+		List<String> nombres = Arrays.asList("PEDRO", "ANA", "MARIA", "JUAN");
+
+		nombres.stream().flatMap(x -> {
+			if (x.startsWith("P")) {
+				return Stream.of(x.toUpperCase());
+			} else if (x.startsWith("M")) {
+				return Stream.of(x.toLowerCase());
+			}
+			return null;
+
+		}).forEach(System.out::println);
+
 	}
 }
